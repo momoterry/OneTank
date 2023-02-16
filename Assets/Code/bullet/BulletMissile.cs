d2655d47f5ea9ad687f15c08af59c3531446b304
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2023-02-16 16:11:21 +0800

    飛彈加速效果調整

--- Assets/Code/bullet/BulletMissile.cs
+++ Assets/Code/bullet/BulletMissile.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class BulletMissile : BulletDir
 {
     public float Acc = 10.0f;
+    public float speedMax = 30.0f;
+
+    public float AccDelay = 0.2f;
 
     protected override void Update()
     {
-        speed += Acc * Time.deltaTime;
+        if (lifeTime - myTime >= AccDelay)
+        {
+            if (speed < speedMax)
+                speed += Acc * Time.deltaTime;
+            else
+                speed = speedMax;
+        }
         base.Update();
     }
 }
